6cde61f398c8df86aaf7dd2dab7fabff1b49fe37
Enmanuel Feliciano <enmanuellemos.f@gmail.com>
2026-06-10 20:44:36 +0200

    creation of classes and methods

---
+++ figures.py
@@ -0,0 +1,104 @@
+from abc import ABC, abstractmethod
+import math
+class Figure(ABC):
+    @abstractmethod
+    def get_name(self):
+        pass
+    @abstractmethod
+    def enter_data(self):
+        pass
+    @abstractmethod
+    def show_result(self):
+        pass
+
+class Figures2D(Figure):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+    @abstractmethod
+    def calculate_perimeter(self):
+        pass
+
+class Figures3D(Figure):
+    @abstractmethod
+    def calculate_volume(self):
+        pass
+    @abstractmethod
+    def calculate_surface_area(self):
+        pass
+
+class Circle(Figures2D):
+    def __init__(self):
+        self.name = 'Circle'
+        self.radio = None
+    def get_name(self):
+        return self.name
+    def enter_data(self):
+        self.radio = float(input(f'enter the data,{self.radio}'))
+        print(self.radio)
+    def calculate_area(self):
+        self.area = math.pi * self.radio ** 2
+    def calculate_perimeter(self):
+        self.perimeter = 2 * self.radio * math.pi
+    def show_result(self):
+        return f'The area of ​​the circle is:{self.area} \n The perimeter of the circle is:{self.perimeter}'
+        
+class Rectangle(Figures2D):
+    def get_name(self):
+        pass
+    def enter_data(self):
+        pass
+    def show_result(self):
+        pass
+    def calculate_area(self):
+        pass
+    def calculate_perimeter(self):
+        pass
+
+class Trapeze(Figures2D):
+    def get_name(self):
+        pass
+    def enter_data(self):
+        pass
+    def show_result(self):
+        pass
+    def calculate_area(self):
+        pass
+    def calculate_perimeter(self):
+        pass
+
+class Cube(Figures3D):
+    def get_name(self):
+        pass
+    def enter_data(self):
+        pass
+    def show_result(self):
+        pass
+    def calculate_volume(self):
+        pass
+    def calculate_surface_area(self):
+        pass
+
+class Cylinder(Figures3D):
+    def get_name(self):
+        pass
+    def enter_data(self):
+        pass
+    def show_result(self):
+        pass
+    def calculate_volume(self):
+        pass
+    def calculate_surface_area(self):
+        pass
+
+class Sphere(Figures3D):
+    def get_name(self):
+        pass
+    def enter_data(self):
+        pass
+    def show_result(self):
+        pass
+    def calculate_volume(self):
+        pass
+    def calculate_surface_area(self):
+        pass
