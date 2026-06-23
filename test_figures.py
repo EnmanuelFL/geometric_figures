2d276e316434b968e42dc3f0ae9efb5a8d9c7edf
Enmanuel Feliciano <enmanuellemos.f@gmail.com>
2026-06-23 17:30:45 -0400

    home of figures interface

--- test_figures.py
+++ test_figures.py
@@ -1,51 +1,5 @@
-# from figures import Circle
-# from figures import Rectangle
-# from figures import Trapeze
-# from figures import Cube
-# from figures import Cylinder
-from figures import Sphere
-
-# FIGURE: CICLE
-# ob_circle = Circle()
-# ob_circle.enter_data()
-# ob_circle.calculate_area()
-# ob_circle.calculate_perimeter()
-# ob_circle.show_result()
-
-# FIGURE: RECTANGLE
-# ob_rectangle = Rectangle()
-# ob_rectangle.enter_data()
-# ob_rectangle.calculate_area()
-# ob_rectangle.calculate_perimeter()
-# ob_rectangle.show_result()
-
-# FIGURE: TRAPEZE
-# ob_trapeze = Trapeze()
-# ob_trapeze.enter_data()
-# ob_trapeze.calculate_area()
-# ob_trapeze.calculate_perimeter()
-# ob_trapeze.show_result()
-
-# FIGURE: CUBE
-# ob_cube = Cube()
-# ob_cube.get_name()
-# ob_cube.enter_data()
-# ob_cube.calculate_volume()
-# ob_cube.calculate_surface_area()
-# ob_cube.show_result()
-
-# FIGURE: CYLINDER 
-# ob_cylinder = Cylinder()
-# ob_cylinder.get_name()
-# ob_cylinder.enter_data()
-# ob_cylinder.calculate_volume()
-# ob_cylinder.calculate_surface_area()
-# ob_cylinder.show_result()
-
-# FIGURE: SPHERE
-ob_sphere = Sphere()
-ob_sphere.get_name()
-ob_sphere.enter_data()
-ob_sphere.calculate_volume()
-ob_sphere.calculate_surface_area()
-ob_sphere.show_result()
+from figures import *
+FIGURES = [Circle,Rectangle, Trapeze, Cube, Cylinder, Sphere]
+while True:
+    for indices, figura in enumerate(FIGURES):
+        print(indices)
